fd2d3054aa1c912f6fd370b7845b7db02b67cae5
Victor Perez <vaperezr@unal.edu.co>
2018-02-25 08:45:02 -0500

    feature: adds method to update the Wallet local state from user input

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -2,10 +2,21 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 export class Wallet extends Component {
+  constructor() {
+    super()
+
+    this.state = { balance: undefined }
+  }
+
+  updateBalance = event => this.setState({ balance: parseInt(event.target.value, 10) })
+
   render() {
     return (
       <div>
         <h3 className="balance">Wallet balance: {this.props.balance}</h3>
+        <hr />
+
+        <input className="input-wallet" onChange={this.updateBalance} />
       </div>
     )
   }
